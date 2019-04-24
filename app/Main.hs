48cb9397120f7b93766a759f610af48956022b79
Roger Bosman <roger_bosman@hotmail.com>
2019-04-24 16:51:00 +0200

    modular apporach

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib
+import           Graphics.Gloss
+import           Lib
+
+import           Data.Array.Accelerate          (Array, DIM1, Elt, Scalar)
+import qualified Data.Array.Accelerate          as A hiding (round)
+import           Data.Array.Accelerate.LLVM.PTX as A
 
 main :: IO ()
 main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeOperators #-}
+
+module Lib where
+
+import           Data.Array.Accelerate              as A
+import           Data.Array.Accelerate.Data.Complex as A
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+-- mandelbrot
+--     :: forall a. (Num a, RealFloat a, FromIntegral Int a, Elt (Complex a))
+--     => Int                                  -- ^ image width
+--     -> Int                                  -- ^ image height
+--     -> Acc (Scalar a)                       -- ^ centre x
+--     -> Acc (Scalar a)                       -- ^ centre y
+--     -> Acc (Scalar a)                       -- ^ view width
+--     -> Acc (Scalar Int32)                   -- ^ iteration limit
+--     -> Acc (Scalar a)                       -- ^ divergence radius
+--     -> Acc (Array DIM2 (Complex a, Int32))
+
+
+
+height, width :: Int
+height = 600
+width = 1050
+
+type IComplex = A.Complex Float
+
+
+juliaSet :: Acc (Array DIM2 IComplex)
+juliaSet = A.generate (A.constant (Z :. height :. width)) calcSingle
+  where
+    calcSingle :: Exp ((Z :. Int) :. Int) -> Exp IComplex
+    calcSingle = undefined
